91d0500783fb0d777712a368ff3f3d779212be40
Tommy <tab114@pitt.edu>
2016-10-10 03:49:36 -0400

    Added BE, CN, BDF(2), and AB(3)

--- integrator.py
+++ integrator.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+import scipy.optimize as opt
 
 
 def _init(x0, t0, tf, h):
@@ -9,14 +10,22 @@ def _init(x0, t0, tf, h):
     x[0] = x0
     return n, t, x
 
-
 def integrate(method, derivatives, params):
-    f, df = derivatives
+    f, dfdt, fx = derivatives
     if method == 'euler':
         return euler(f, *params)
     elif method == 'ts2':
-        return ts2(f, df, *params)
-    raise ValueError('Unrecognized integration method: ' + method)
+        return ts2(f, dfdt, *params)
+    elif method == 'be':
+        return backwards_euler(f, fx, *params)
+    elif method == 'cn':
+        return trapezoidal(f, fx, *params)
+    elif method == 'bdf2':
+        return bdf2(f, fx, *params)
+    elif method == 'ab3':
+        return ab3 (f, fx, *params)
+    else:
+        raise ValueError('Unrecognized integration method: ' + method)
 
 # x[n+1] - x[n] = h*f[n]
 def euler(f, x0, t0, tf, h):
@@ -26,24 +35,46 @@ def euler(f, x0, t0, tf, h):
     return t, x
 
 # x[n+1]- x[n]= h*f[n] + 1/2*h^2*f'[n]
-def ts2(f, df, x0, t0, tf, h):
+def ts2(f, dfdt, x0, t0, tf, h):
     n, t, x = _init(x0, t0, tf, h)
     for i in range(n):
-        x[i+1] = x[i] + h*f(t[i], x[i]) + 0.5*h*h*df(t[i], x[i])
+        x[i+1] = x[i] + h*f(t[i], x[i]) + 1/2*h*h*dfdt(t[i], x[i])
     return t, x
 
 # x[n+1] - x[n] = h*f[n+1]
-def backwards_euler():
-    pass
+def backwards_euler(f, fx, x0, t0, tf, h):
+    n, t, x = _init(x0, t0, tf, h)
+    for i in range(n):
+        g = lambda u: u - x[i] - h*f(t[i+1], u)
+        dg = lambda u: 1 - h*fx(t[i+1], u)
+        x[i+1] = opt.newton(g, x[i], dg, tol=1e-6)
+    return t, x
+
 
 # x[n+1] - x[n] = 1/2*h*(f[n+1]+f[n])
-def trapezoidal():
-    pass
+def trapezoidal(f, fx, x0, t0, tf, h):
+    n, t, x = _init(x0, t0, tf, h)
+    for i in range(n):
+        g = lambda u: u - x[i] - 1/2*h*(f(t[i+1], u) + f(t[i], x[i]))
+        dg = lambda u: 1 - 1/2*h*fx(t[i+1], u)
+        x[i+1] = opt.newton(g, x[i], dg, tol=1e-6)
+    return t, x
 
 # x[n+2] - 4/3*x[n+1] + 1/3*x[n] = 2/3*h*f[n+2]
-def bdf2():
-    pass
+def bdf2(f, fx, x0, t0, tf, h):
+    n, t, x = _init(x0, t0, tf, h)
+    x[1] = trapezoidal(f, fx, x0, t0, t0 + h, h)[1][1]
+    for i in range(n-1):
+        g = lambda u: u - 4/3*x[i+1] + 1/3*x[i] - 2/3*h*f(t[i+2], u)
+        dg = lambda u: 1 - 2/3*h*fx(t[i+2], u)
+        x[i+2] = opt.newton(g, x[i+1], dg, tol=1e-6)
+    return t, x
 
-# x[n+3] - x[n+2] = 1/12*h*(23*f[n+2] - 48*f[n+1] 5*f[n])
-def ab3():
-    pass
+# x[n+3] - x[n+2] = 1/12*h*(23*f[n+2] - 48*f[n+1] + 5*f[n])
+def ab3(f, fx, x0, t0, tf, h):
+    n, t, x = _init(x0, t0, tf, h)
+    x[1:2] = trapezoidal(f, fx, x0, t0, t0 + 2*h, h)[1][1:2]
+    for i in range(n-2):
+        x[i+3] = x[i+2] + 1/12*h*(23*f(t[i+2], x[i+2]) - 48*f(t[i+1], x[i+1]) + 5*f(t[i], x[i]))
+    return t, x
+
--- ode_test.py
+++ ode_test.py
@@ -9,27 +9,35 @@ import numpy as np
 def f(k, t, x):
     return k*x
 
-def df(k, t, x):
+def dfdt(k, t, x):
     return k*k*x
+
+def fx(k, t, x):
+    return k
 
 def g(t, x):
     return math.cos(t)*x
 
-def dg(t, x):
+def dgdt(t, x):
     return (math.cos(t)**2 - math.sin(t))*x
 
+def gx(t, x):
+    return math.cos(t)
+
 def run_test_problem(test_func, description, integration_params):
-    x_exact, f_test, df_test = test_func
+    x_exact, f_test, dfdt_test, fx_test = test_func
     method, test_name = description
     hs, x0, t0, tf = integration_params
     plt.figure()
     plt.title("{} on {}".format(*description))
     for h in hs:
-        derivatives = f_test, df_test
+        derivatives = f_test, dfdt_test, fx_test
         params = x0, t0, tf, h
         t, x = integrator.integrate(method, derivatives, params)
+        xe = x_exact(t)
+        print method, test_name, h, np.max(np.abs(xe - x))
         plt.plot(t, x, label='h = {}'.format(h))
-    plt.plot(t, x_exact(t), label='exact')
+    plt.plot(t, xe, label='exact')
     plt.legend()
 
 
@@ -37,17 +45,17 @@ def main():
     x0 = 1
     t0, tf = 0, 5
 
-    for method in 'euler', 'ts2':
+    for method in ('be', 'cn', 'bdf2', 'ab3'):
         ks = [-1, -10, -50]
         hs = [0.5, 0.1, 0.01]
         for k in ks:
-            test_func = lambda t: np.exp(k*t), partial(f,k), partial(df,k)
+            test_func = lambda t: np.exp(k*t), partial(f,k), partial(dfdt,k), partial(fx,k)
             description = method, 'exp({}t)'.format(k)
             integration_params = hs, x0, t0, tf
             run_test_problem(test_func, description, integration_params)
 
         hs = [0.2, 0.1, 0.05, 0.025]
-        test_func = lambda t: np.exp(np.sin(t)), g, dg
+        test_func = lambda t: np.exp(np.sin(t)), g, dgdt, gx
         description = method, 'exp(sin(t))'
         integration_params = hs, x0, t0, tf
         run_test_problem(test_func, description, integration_params)
